dcdb1e6b3f810d59442621f9e345176536735856
Soumik <griimick@gmail.com>
2022-12-03 03:51:28 +0530

    improve day1 error handling

--- day01/src/main.rs
+++ day01/src/main.rs
@@ -1,14 +1,18 @@
-pub fn main() {
+// proper way to propagate errors
+use std::error::Error;
+
+pub fn main() -> Result<(), Box<dyn Error>> {
     let mut elf_calories = include_str!("../input.txt")
         .split("\n\n")
         .map(|elf_str| { 
             elf_str
                 .split("\n")
                 .filter(|s| !s.is_empty())
-                .map(|n| n.parse::<u32>().unwrap())
-                .sum::<u32>()
+                .map(|n| n.parse::<u32>())
+                .collect::<Result<Vec<_>, _>>()
+                .map(|counts| counts.into_iter().sum())
         })
-        .collect::<Vec<u32>>();
+        .collect::<Result<Vec<u32>,_>>()?;
 
     elf_calories.sort_by(|a, b| b.cmp(a));
 
@@ -17,4 +21,6 @@ pub fn main() {
 
     let part2 = elf_calories[..3].into_iter().sum::<u32>();
     println!("part2: {part2}");
+
+    Ok(())
 }
