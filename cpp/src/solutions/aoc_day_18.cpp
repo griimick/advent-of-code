c3c27ac6061e1e3cb67c76daa61966dba1dc8031
Soumik <hey@griim.xyz>
2022-12-18 20:59:09 +0530

    chore: lint

--- cpp/src/solutions/aoc_day_18.cpp
+++ cpp/src/solutions/aoc_day_18.cpp
@@ -13,7 +13,7 @@
 
 using namespace std;
 
-AocDay18::AocDay18():AocDay(0)
+AocDay18::AocDay18() : AocDay(0)
 {
 }
 
@@ -39,12 +39,12 @@ vector<string> AocDay18::read_input(string filename)
 }
 
 const vector<array<int, 3>> directions = {
-    { 1,  0,  0},
-    { 0,  1,  0},
-    { 0,  0,  1},
-    {-1,  0,  0},
-    { 0, -1,  0},
-    { 0,  0, -1},
+    {1, 0, 0},
+    {0, 1, 0},
+    {0, 0, 1},
+    {-1, 0, 0},
+    {0, -1, 0},
+    {0, 0, -1},
 };
 
 string AocDay18::part1(string filename, vector<string> extra_args)
@@ -52,7 +52,8 @@ string AocDay18::part1(string filename, vector<string> extra_args)
     vector<string> data = read_input(filename);
     unordered_set<string> cubes(data.begin(), data.end());
     long sides = 0;
-    for (auto& cube: cubes) {
+    for (auto &cube : cubes)
+    {
         int connected = 0;
         stringstream ss(cube);
         int x, y, z;
@@ -66,12 +67,14 @@ string AocDay18::part1(string filename, vector<string> extra_args)
             getline(ss, substr, ',');
             z = stol(substr);
         }
-        for (auto& dir: directions) {
-            if (cubes.find(to_string(x + dir[0]) + ',' + to_string(y + dir[1]) + ',' + to_string(z + dir[2])) != cubes.end()) {
+        for (auto &dir : directions)
+        {
+            if (cubes.find(to_string(x + dir[0]) + ',' + to_string(y + dir[1]) + ',' + to_string(z + dir[2])) != cubes.end())
+            {
                 connected++;
             }
         }
-        sides += 6 - connected; 
+        sides += 6 - connected;
     }
     ostringstream out;
     out << sides;
@@ -98,7 +101,8 @@ string AocDay18::part2(string filename, vector<string> extra_args)
     */
     long xmax = LONG_MIN, ymax = LONG_MIN, zmax = LONG_MIN;
     long xmin = LONG_MAX, ymin = LONG_MAX, zmin = LONG_MAX;
-    for (auto& cube: cubes) {
+    for (auto &cube : cubes)
+    {
         stringstream ss(cube);
         long x, y, z;
         while (ss.good())
@@ -119,41 +123,52 @@ string AocDay18::part2(string filename, vector<string> extra_args)
         zmin = min(zmin, z);
     }
     /* Following are the outer bounds */
-    xmax += 1; ymax += 1; zmax += 1;
-    xmin -= 1; ymin -= 1; zmin -= 1;
+    xmax += 1;
+    ymax += 1;
+    zmax += 1;
+    xmin -= 1;
+    ymin -= 1;
+    zmin -= 1;
 
     long sides = 0;
-    stack<array<long ,3>> stk;
+    stack<array<long, 3>> stk;
     unordered_set<string> watered;
     stk.push({xmax, ymax, zmax});
 
-    while (!stk.empty()) {
-        auto curr = stk.top(); stk.pop();
-        string  pos = to_string(curr[0]) + ',' + to_string(curr[1]) + ',' + to_string(curr[2]);
+    while (!stk.empty())
+    {
+        auto curr = stk.top();
+        stk.pop();
+        string pos = to_string(curr[0]) + ',' + to_string(curr[1]) + ',' + to_string(curr[2]);
 
-        if (watered.find(pos) != watered.end()) {
+        if (watered.find(pos) != watered.end())
+        {
             continue;
         }
 
         watered.insert(pos);
 
-        for (auto& dir : directions) {
+        for (auto &dir : directions)
+        {
             long x = curr[0] + dir[0];
             long y = curr[1] + dir[1];
             long z = curr[2] + dir[2];
             string pos = to_string(x) + ',' + to_string(y) + ',' + to_string(z);
 
             // out of bounds
-            if (x > xmax || y > ymax || z > zmax || x < xmin || y < ymin || z < zmin) {
+            if (x > xmax || y > ymax || z > zmax || x < xmin || y < ymin || z < zmin)
+            {
                 continue;
             }
-            
+
             // adjecent lava cube
             if (cubes.find(pos) != cubes.end())
             {
                 sides++;
-            //  not lava but not watered
-            } else {
+                //  not lava but not watered
+            }
+            else
+            {
                 stk.push({x, y, z});
             }
         }
